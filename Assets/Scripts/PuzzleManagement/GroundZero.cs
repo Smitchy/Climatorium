0bdd93feec4e81ff97bfb8e6f39af685a9d30f6a
KAdams01 <31958267+KAdams01@users.noreply.github.com>
2020-03-12 11:24:39 +0100

    It's here.

---
+++ Assets/Scripts/PuzzleManagement/GroundZero.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundZero : MonoBehaviour
+{
+    private int infectNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(BeginOutbreak());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator BeginOutbreak()
+    {
+        GameObject go = new GameObject("InfectedChinaman" + infectNumber);
+        infectNumber++;
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(BeginOutbreak());
+        StartCoroutine(BeginOutbreak());
+    }
+}
