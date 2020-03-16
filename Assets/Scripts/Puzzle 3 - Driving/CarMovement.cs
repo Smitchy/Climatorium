ddad2e66e8739283453a3bd5629b111b06b106ff
KAdams01 <31958267+KAdams01@users.noreply.github.com>
2020-03-16 12:47:52 +0100

    Setup for driving puzzle
    Removed the coronavirus.
    Added a basic setup for the driving puzzle with basic objects and script.

---
+++ Assets/Scripts/Puzzle 3 - Driving/CarMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    private GameObject grabbedBy;
+    private Vector3 previousFramePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(grabbedBy != null)
+        {
+            ProcessMovement(grabbedBy.transform.position);
+        }
+    }
+
+    public void Grabbed(GameObject go)
+    {
+        grabbedBy = go;
+    }
+    public void Released()
+    {
+        grabbedBy = null;
+    }
+    //TODO checks for movement relating to maze.
+    //Potential grid based movement
+    //Resets if hitting "water"
+    public void ProcessMovement(Vector3 position)
+    {
+        if(position.x != previousFramePosition.x)
+        {
+            Vector3 updatedX = new Vector3(previousFramePosition.x - position.x , position.y, previousFramePosition.z);
+            gameObject.transform.position = updatedX;
+        }
+        if(position.z != previousFramePosition.z)
+        {
+            Vector3 updatedZ = new Vector3(position.x, position.y, previousFramePosition.z - position.z);
+            gameObject.transform.position = updatedZ;
+        }
+        previousFramePosition = position;
+    }
+}
