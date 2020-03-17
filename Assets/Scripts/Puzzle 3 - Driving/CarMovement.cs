ba227bcbfcd68f9cf6ef176ffbff7e5289a1c22d
KAdams01 <31958267+KAdams01@users.noreply.github.com>
2020-03-17 13:33:32 +0100

    Potential maze completion
    Needs VR testing/alternative testing with VRTK tools instead. The same logic may be doable with colliders instead.

--- Assets/Scripts/Puzzle 3 - Driving/CarMovement.cs
+++ Assets/Scripts/Puzzle 3 - Driving/CarMovement.cs
@@ -6,13 +6,7 @@ public class CarMovement : MonoBehaviour
 {
     private GameObject grabbedBy;
     private Vector3 previousFramePosition;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if(grabbedBy != null)
@@ -36,14 +30,80 @@ public class CarMovement : MonoBehaviour
     {
         if(position.x != previousFramePosition.x)
         {
-            Vector3 updatedX = new Vector3(previousFramePosition.x - position.x , position.y, previousFramePosition.z);
-            gameObject.transform.position = updatedX;
+            if(previousFramePosition.x > position.x)
+            {
+                if (CheckHorizontalMovement(true))
+                {
+                    Debug.Log("Positive Horizontal");
+                    Vector3 updatedX = new Vector3(gameObject.transform.position.x - (previousFramePosition.x - position.x), gameObject.transform.position.y, gameObject.transform.position.z);
+                    gameObject.transform.position = updatedX;
+                }
+            }
+            else
+            {
+                if (CheckHorizontalMovement(false))
+                {
+                    Debug.Log("Negative Horizontal");
+                    Vector3 updatedX = new Vector3(gameObject.transform.position.x - (previousFramePosition.x - position.x), gameObject.transform.position.y, gameObject.transform.position.z);
+                    gameObject.transform.position = updatedX;
+                }
+            }
         }
         if(position.z != previousFramePosition.z)
         {
-            Vector3 updatedZ = new Vector3(position.x, position.y, previousFramePosition.z - position.z);
-            gameObject.transform.position = updatedZ;
+            if(previousFramePosition.z > position.z)
+            {
+                if (CheckVerticalMovement(true))
+                {
+                    Debug.Log("Positive Vertical");
+                    Vector3 updatedZ = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - (previousFramePosition.z - position.z));
+                    gameObject.transform.position = updatedZ;
+                }
+            }
+            else
+            {
+                if (CheckVerticalMovement(false))
+                {
+                    Debug.Log("Negative Vertical");
+                    Vector3 updatedZ = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z - (previousFramePosition.x - position.z));
+                    gameObject.transform.position = updatedZ;
+                }
+            }
         }
         previousFramePosition = position;
+    }
+    private bool CheckHorizontalMovement(bool forward)
+    {
+        Vector3 direction = forward ? gameObject.transform.forward : -gameObject.transform.forward;
+        RaycastHit hit;
+        if(Physics.Raycast(new Ray(gameObject.transform.position, direction), out hit, 0.55f))
+        {
+            if(hit.collider.tag == "Maze")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private bool CheckVerticalMovement(bool up)
+    {
+        Vector3 direction = up ? -gameObject.transform.right : gameObject.transform.right;
+        RaycastHit hit;
+        if (Physics.Raycast(new Ray(gameObject.transform.position, direction), out hit, 0.45f))
+        {
+            if (hit.collider.tag == "Maze")
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
