8df52d1b2f16b0d92edc51f7abd1f7e846a67b56
RuneSChr <42325843+RuneSChr@users.noreply.github.com>
2020-03-18 21:29:31 +0100

    wrenchPuzzle draft
    missing obstacle course

---
+++ Assets/Scripts/MiscTest/OpacityChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OpacityChanger : MonoBehaviour
+{
+
+    Material test;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        test = gameObject.GetComponent<MeshRenderer>().material;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Material target, green;
+    public void UpdateOpacity(float alphaValue)
+    {
+        // Color color = target.color;
+        // color.a = alphaValue;
+        // target.color = color;
+        gameObject.GetComponent<MeshRenderer>().material = target;
+    }
+
+    public void ChangeToGreen()
+    {
+        gameObject.GetComponent<MeshRenderer>().material = green;
+    }
+}
