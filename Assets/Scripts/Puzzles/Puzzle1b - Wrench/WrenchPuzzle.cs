daeaf75c28836e4a44cac4f66a9c2413aedf4c5b
RuneSChr <42325843+RuneSChr@users.noreply.github.com>
2020-03-19 11:00:05 +0100

    implementation

--- Assets/Scripts/Puzzles/Puzzle1b - Wrench/WrenchPuzzle.cs
+++ Assets/Scripts/Puzzles/Puzzle1b - Wrench/WrenchPuzzle.cs
@@ -3,77 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class WrenchPuzzle : MonoBehaviour
+public class WrenchPuzzle : iPuzzle
 {
 
-    // private Vector3 startPos;
-
-    // private UnityEvent ResetWrenchEvent;
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     if(ResetWrenchEvent == null)
-    //     {
-    //         ResetWrenchEvent = new UnityEvent();
-    //     }
-    //     ResetWrenchEvent.AddListener(ResetWrench);
-    //     ResetWrenchEvent.AddListener(HapticFeedBack);
-
-    //     startPos = transform.position;
-    // }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.B))
-        {
-           
-            for(int i = 0 ; i < 40; i++)
-            {
-                transform.Translate(Vector3.left * Time.deltaTime);
-
-            }
-        }
-    }
-
-    //reset the position of the wrench
-    void ResetWrench()
-    {
-        transform.position = startPos;
-    }
-
-    //Vibrate the controller when failed - should probably be somewhere else
-    void HapticFeedBack()
-    {
-
-    }
-
-    private void OnTriggerExit(Collider other) 
-    {
-        if(other.name == "Interactable.Primary_Grab.Secondary_Swap")
-        {
-            print("test");
-            TriggerEvent();
-        }
-    }
-
-    private void OnTriggerEnter(Collider other) 
-    {
-        if(other.name == "Interactable.Primary_Grab.Secondary_Swap")
-        {
-            print("enter");
-        }
-    }
-
-    public void TriggerEvent()
-    {
-        ResetWrenchEvent.Invoke();
-    }
-}
-
-
-public class WrenchPuzzle1 : iPuzzle
-{
     private UnityEvent ResetWrenchEvent;
     private Vector3 startPos;
     public override void SetUp()
@@ -141,4 +73,73 @@ public class WrenchPuzzle1 : iPuzzle
     {
         ResetWrenchEvent.Invoke();
     }
+    // private Vector3 startPos;
+
+    // private UnityEvent ResetWrenchEvent;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+    //     if(ResetWrenchEvent == null)
+    //     {
+    //         ResetWrenchEvent = new UnityEvent();
+    //     }
+    //     ResetWrenchEvent.AddListener(ResetWrench);
+    //     ResetWrenchEvent.AddListener(HapticFeedBack);
+
+    //     startPos = transform.position;
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if(Input.GetKeyDown(KeyCode.B))
+    //     {
+           
+    //         for(int i = 0 ; i < 40; i++)
+    //         {
+    //             transform.Translate(Vector3.left * Time.deltaTime);
+
+    //         }
+    //     }
+    // }
+
+    // //reset the position of the wrench
+    // void ResetWrench()
+    // {
+    //     transform.position = startPos;
+    // }
+
+    // //Vibrate the controller when failed - should probably be somewhere else
+    // void HapticFeedBack()
+    // {
+
+    // }
+
+    // private void OnTriggerExit(Collider other) 
+    // {
+    //     if(other.name == "Interactable.Primary_Grab.Secondary_Swap")
+    //     {
+    //         print("test");
+    //         TriggerEvent();
+    //     }
+    // }
+
+    // private void OnTriggerEnter(Collider other) 
+    // {
+    //     if(other.name == "Interactable.Primary_Grab.Secondary_Swap")
+    //     {
+    //         print("enter");
+    //     }
+    // }
+
+    // public void TriggerEvent()
+    // {
+    //     ResetWrenchEvent.Invoke();
+    // }
+}
+
+
+public class WrenchPuzzle1 : iPuzzle
+{
+    
 }
