5a3445132499ca033c9ad91be0732007fbc5fc8f
KAdams01 <31958267+KAdams01@users.noreply.github.com>
2020-03-28 11:05:54 +0100

    Movingscript

---
+++ Assets/Scripts/Puzzle 3 - Driving/MoveObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    private Vector3 startPos;
+
+    [SerializeField]
+    private Transform toPos;
+    [SerializeField]
+    private float speed, smoothTime;
+
+    private Vector3 vel = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.SmoothDamp(transform.position, toPos.position,ref vel, smoothTime, step);
+        if(transform.position == toPos.position)
+        {
+            toPos.position = startPos;
+            startPos = transform.position;
+        }
+    }
+}
