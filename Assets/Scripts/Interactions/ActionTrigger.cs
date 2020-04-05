64c4f3d6f698811a5239286a85e4fee6d91d347c
Smitchy <life.limergmail.com>
2020-04-05 18:44:02 +0200

    Interactions done and alarm clock

---
+++ Assets/Scripts/Interactions/ActionTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK.Prefabs.Interactions.Interactables;
+using VRTK.Prefabs.Interactions.Interactors;
+using Zinnia.Data.Collection.List;
+using Zinnia.Tracking.Collision.Active;
+
+public class ActionTrigger : MonoBehaviour
+{
+
+    private List<InteractableFacade> interactableFacades = new List<InteractableFacade>();
+
+    public void ObjectAction()
+    {
+        if (interactableFacades.Count > 0)
+        {
+            foreach(InteractableFacade interactor in interactableFacades)
+            {
+                interactor.gameObject.GetComponent<IAction>().DoAction();
+            }
+        }
+    }
+
+    public void AddInteractor(InteractableFacade interactor)
+    {
+        interactableFacades.Add(interactor);
+    }
+
+    public void RemovedeInteractor(InteractableFacade interactor)
+    {
+        interactableFacades.Remove(interactor);
+    }
+}
