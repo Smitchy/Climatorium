9f16d51c6d238d4467c5253ba5e5456e0f3d90f3
Smitchy <life.limergmail.com>
2020-04-06 11:29:12 +0200

    enabled clock and did some minor clean up

---
+++ Assets/Scripts/Puzzle 3 - Driving/YLockTransform.cs
@@ -0,0 +1,19 @@
+﻿
+    using UnityEngine;
+
+    /// <summary>
+    /// Updates the transform position of the target to match the source.
+    /// </summary>
+    public class YLockTransform : Zinnia.Tracking.Follow.Modifier.Property.PropertyModifier
+{
+        /// <summary>
+        /// Modifies the target position to match the given source position.
+        /// </summary>
+        /// <param name="source">The source to utilize in the modification.</param>
+        /// <param name="target">The target to modify.</param>
+        /// <param name="offset">The offset of the target against the source when modifying.</param>
+        protected override void DoModify(GameObject source, GameObject target, GameObject offset = null)
+        {
+                target.transform.position = new Vector3(source.transform.position.x, target.transform.position.y, source.transform.position.z);
+        }
+    }
