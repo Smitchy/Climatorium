b62c3c9d382fabf11b4124d4aa0b51edf72d2db3
RuneSChr <42325843+RuneSChr@users.noreply.github.com>
2020-04-09 14:30:47 +0200

    started sound manager
    playing with implementation ideas to make it smort

---
+++ Assets/Scripts/MiscTest/SnapzoneTest/MoveToSnapZone.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoveToSnapZone : MonoBehaviour
+{
+    private bool grabbed, insideTrigger;
+    public TMP_Text test;
+    [SerializeField]
+    private GameObject snapZoneTrigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Snap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Snap();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "SnapZoneTrigger" || other.name == "ThingaMabob")
+        {
+            snapZoneTrigger = other.gameObject;
+            insideTrigger = true;
+            test.text = "inside snapzone, true";
+        }
+        else
+        {
+            insideTrigger = false;
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        test.text = other.gameObject.name;
+        if (other.name == "SnapZoneTrigger")
+        {
+            snapZoneTrigger = other.gameObject;
+            insideTrigger = false;
+            test.text = "inside snapzone, false";
+        }
+       
+    }
+    public void Snap()
+    {
+        if (!grabbed)
+        {
+            StartCoroutine(KeepInPlace());
+            
+            //snapZoneTrigger.transform.position = ;
+
+        }
+    }
+
+    //set the grabbed boolean to opposite on method call
+    public void ChangeState()
+    {
+        grabbed = !grabbed;
+        test.text = grabbed + "";
+    }
+
+   public IEnumerator KeepInPlace()
+    {
+        while (true)
+        {
+            transform.position = snapZoneTrigger.transform.position;
+            yield return new WaitForSeconds(0.01f);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/SoundManager/PlaySound.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+
+public class PlaySound : MonoBehaviour
+{
+
+    private bool playing;
+    private AudioSource soundSrc, newPlayer;
+    public Coroutine SoundRoutine;
+
+    [SerializeField]
+    private AudioClip clip;
+    [SerializeField]
+    private List<AudioClip> sounds;
+    [SerializeField]
+    private Vector3 soundPosition;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundSrc = gameObject.AddComponent<AudioSource>();
+        StartPlayer(transform.position,clip);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartPlayer(Vector3 soundPositionIn3DSpace, AudioClip sound)
+    {
+        if (!playing)
+        {
+            playing = !playing;
+            SoundRoutine = StartCoroutine(Player(soundPositionIn3DSpace,sound));
+        }
+    }
+
+    private IEnumerator Player(Vector3 position, AudioClip sound)
+    {
+        
+        transform.position = position;
+        soundSrc.PlayOneShot(sound);
+        print("hey before");
+        yield return new WaitForSeconds(sound.length + 0.02f);
+        print("hey after");
+        
+        playing = !playing;
+    }
+}
