baaec468da8f1ecc1a67d1dc027759d06f24dbb8
KAdams01 <31958267+KAdams01@users.noreply.github.com>
2020-04-15 15:47:42 +0200

    Added menus and some testing of headset removal

---
+++ Assets/Scripts/CurrentState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CurrentState
+{
+    public static StateEnum currentState = StateEnum.PuzzleInProgress;
+
+    private static StateEnum returnPoint;
+
+    public static void NextState()
+    {
+        currentState = currentState + 1;
+    }
+
+    //For menus etc to be able to easily set a state to return to
+    public static void SetStateStart(StateEnum state)
+    {
+        returnPoint = currentState;
+        currentState = state;
+    }
+    //when menus are closed, return to state before menu was opened
+    public static void SetStateEnd()
+    {
+        currentState = returnPoint;
+    }
+    //sets a custom state.
+    public static void SetCustomState(StateEnum state)
+    {
+        currentState = state;
+    }
+}
---
+++ Assets/Scripts/MenuManagement/PauseMenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public Canvas pauseMenu;
+    private bool activated;
+
+    public void ToggleMenu()
+    {
+
+    }
+    public void ActivatePause()
+    {
+        if (!activated)
+        {
+            /*Time.timeScale = 0;*/
+            pauseMenu.gameObject.SetActive(true);
+            CurrentState.SetStateStart(StateEnum.PauseMenu);
+            Debug.Log("Activating pause");
+            activated = true;
+        }
+
+    }
+    public void DeactivatePause()
+    {
+        if (activated)
+        {
+            /*Time.timeScale = 1;*/
+            pauseMenu.gameObject.SetActive(false);
+            CurrentState.SetStateEnd();
+            Debug.Log("Deactivating pause");
+            activated = false;
+        }
+
+    }
+}
