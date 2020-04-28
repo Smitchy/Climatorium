3c075b327d27a9062c9a0b6f939d2aa2c3a52b7e
KAdams01 <31958267+KAdams01@users.noreply.github.com>
2020-04-28 11:44:18 +0200

    Consolodated all menu scripts into one

---
+++ Assets/Scripts/MenuManagement/GenericMenuManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericMenuManager : MonoBehaviour
+{
+    private bool activated;
+    private int startingMask;
+    public Camera VRCamera;
+    public GameObject MainMenu, PauseMenu, PlayerChangeMenu, EndGameMenu, Options;
+    private GameObject currentMenu;
+
+    private void Awake()
+    {
+        startingMask = VRCamera.cullingMask;
+    }
+
+    public void ActivateMenu(StateEnum menu)
+    {
+        if (!activated)
+        {
+            ShowUIOnly();
+            Time.timeScale = 0;
+            switch (menu)
+            {
+                case StateEnum.MainMenu:
+                    MainMenu.SetActive(true);
+                    currentMenu = MainMenu;
+                    CurrentState.SetStateStart(StateEnum.MainMenu);
+                    break;
+                case StateEnum.PauseMenu:
+                    PauseMenu.SetActive(true);
+                    currentMenu = PauseMenu;
+                    CurrentState.SetStateStart(StateEnum.PauseMenu);
+                    break;
+                case StateEnum.PlayerChange:
+                    PlayerChangeMenu.SetActive(true);
+                    currentMenu = PlayerChangeMenu;
+                    CurrentState.SetStateStart(StateEnum.PlayerChange);
+                    break;
+                case StateEnum.EndGame:
+                    EndGameMenu.SetActive(true);
+                    currentMenu = EndGameMenu;
+                    CurrentState.SetStateStart(StateEnum.EndGame);
+                    break;
+                case StateEnum.OptionsMenu:
+                    currentMenu.SetActive(false);
+                    Options.SetActive(true);
+                    currentMenu = Options;
+                    CurrentState.SetStateStart(StateEnum.OptionsMenu);
+                    break;
+            }            
+            Debug.Log("Activating " + menu);
+            activated = true;
+        }
+    }
+    public void DeactivateMenu()
+    {
+        if (activated && currentMenu != null)
+        {
+            currentMenu.SetActive(false);
+            currentMenu = null;
+            ShowEverything();
+            Time.timeScale = 1;
+            Debug.Log("Deactivating " + CurrentState.currentState);
+            CurrentState.SetStateEnd();
+            activated = false;
+        }
+    }
+
+    private void ShowUIOnly()
+    {
+        VRCamera.cullingMask |= 1 << LayerMask.NameToLayer("UI");
+        Debug.Log("Showing UI");
+    }
+    private void ShowEverything()
+    {
+        VRCamera.cullingMask = startingMask;
+    }
+}
--- Assets/Scripts/MenuManagement/MenuManager.cs
+++ Assets/Scripts/MenuManagement/MenuManager.cs
@@ -5,40 +5,18 @@ using UnityEngine.XR;
 
 public class MenuManager : MonoBehaviour
 {
-    public MainMenu MMM;
-    public PauseMenuManager PMM;
-    public PlayerChangeManager PCM;
-    public EndGameManager EGM;
-    public Camera VRCamera;
-    private int startingMask;
-    
-
+    private GenericMenuManager gMM;
     private UserPresenceState previousUserPresence;
 
     private void Awake()
     {
         previousUserPresence = XRDevice.userPresence;
-        startingMask = VRCamera.cullingMask;
+        gMM = GetComponent<GenericMenuManager>();
     }
 
     public void DisplayMenu(StateEnum state)
     {
-        ShowUIOnly();
-        switch (state)
-        {
-            case StateEnum.MainMenu:
-                ToggleMainMenu();
-                break;
-            case StateEnum.PauseMenu:
-                TogglePauseMenu();
-                break;
-            case StateEnum.PlayerChange:
-                TogglePlayerChange();
-                break;
-            case StateEnum.EndGame:
-                ToggleEndGameMenu();
-                break;
-        }
+        gMM.ActivateMenu(state);
     }
     private void Update()
     {
@@ -46,37 +24,12 @@ public class MenuManager : MonoBehaviour
         {
             if (XRDevice.userPresence == UserPresenceState.NotPresent)
             {
-                ShowUIOnly();
-                PMM.ActivatePause();
+                if (CurrentState.currentState == StateEnum.PuzzleInProgress)
+                {
+                    DisplayMenu(StateEnum.PauseMenu);
+                }
             }
             previousUserPresence = XRDevice.userPresence;
         }
     }
-
-    private void ToggleMainMenu()
-    {
-
-    }
-    private void TogglePlayerChange()
-    {
-
-    }
-    private void TogglePauseMenu()
-    {
-        PMM.ToggleMenu();
-    }
-    private void ToggleEndGameMenu()
-    {
-
-    }
-    private void ShowUIOnly()
-    {
-        VRCamera.cullingMask |= 1 << LayerMask.NameToLayer("UI");
-        Debug.Log("Showing UI");
-    }
-    private void ShowEverything()
-    {
-        VRCamera.cullingMask = startingMask;
-    }
-
 }
