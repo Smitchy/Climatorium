9ee6c9efaaad48495877c8a679d290166a97a347
LZagata <268054@via.dk>
2020-05-08 14:57:23 +0200

    Simple persistence manager created
    As this class does not derive from Monobehabiour, you don't have to have it initialized as a component on any game object, just call the class from wherever you like in your scripts

---
+++ Assets/Scripts/PersistenceManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public static class PersistenceManager
+{
+    private static int currentScore;
+    private static int highScore;
+
+    public static void Initialize()
+    {
+        currentScore = 0;
+        highScore = GetHighScore();
+    }
+
+    public static void SaveScore(int score)
+    {
+        currentScore += score;
+
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+            SetNewHighScore(highScore);
+        } 
+    }
+
+    public static int GetHighScore()
+    {
+        return PlayerPrefs.GetInt("HighScore", 0);
+    }
+
+    private static void SetNewHighScore(int score)
+    {
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+}
