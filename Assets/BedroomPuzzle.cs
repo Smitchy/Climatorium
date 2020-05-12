ce7479f8530c1296235ecb4dc73e5b6789877673
KAdams01 <31958267+KAdams01@users.noreply.github.com>
2020-05-12 16:28:40 +0200

    Stash

---
+++ Assets/BedroomPuzzle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedroomPuzzle : iPuzzle
+{
+    public override void SetUp()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void TearDown()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/WaterpuzzleIPuzzle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterpuzzleIPuzzle : iPuzzle
+{
+    public override void SetUp()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public override void TearDown()
+    {
+        gameObject.SetActive(true);
+    }
+
+}
