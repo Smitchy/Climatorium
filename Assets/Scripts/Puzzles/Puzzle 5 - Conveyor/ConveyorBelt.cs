b1def68ed6439c6469510f867e8686e45c2d6450
Smitchy <life.limergmail.com>
2020-05-23 14:45:09 +0200

    conveyer puzzle prefab

--- Assets/Scripts/Puzzles/Puzzle 5 - Conveyor/ConveyorBelt.cs
+++ Assets/Scripts/Puzzles/Puzzle 5 - Conveyor/ConveyorBelt.cs
@@ -6,19 +6,12 @@ public class ConveyorBelt : MonoBehaviour
 {
     [SerializeField]
     private float speed;
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.CompareTag("Food"))
-        {
-            other.transform.Translate(speed * transform.forward, this.transform);
-        }
-    }
 
     private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Food"))
         {
-            collision.gameObject.transform.Translate(speed * transform.forward, this.transform);
+            collision.gameObject.transform.Translate(speed * transform.forward, Space.World);
         }
     }
 }
