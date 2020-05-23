253076a3ac1c9c1db3cc40fd57085ef20d5a314c
Smitchy <life.limergmail.com>
2020-05-23 11:36:26 +0200

    Alarm clock fixet in main scene

--- Assets/Scripts/BedRoom/AlarmClockAction.cs
+++ Assets/Scripts/BedRoom/AlarmClockAction.cs
@@ -5,15 +5,8 @@ using UnityEngine;
 public class AlarmClockAction : MonoBehaviour, IAction
 {
 
-    private AudioSource audioSource;
-
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
     public void DoAction()
     {
-        audioSource.Stop();
+        GetComponentInChildren<AudioSource>().Stop();
     }
 }
