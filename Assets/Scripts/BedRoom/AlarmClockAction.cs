ebdb89b30fdb9afc97421abc3a8b5639f15cd54d
Smitchy <life.limergmail.com>
2020-05-23 11:50:34 +0200

    Stopping alarm clock goes to next puzzle

--- Assets/Scripts/BedRoom/AlarmClockAction.cs
+++ Assets/Scripts/BedRoom/AlarmClockAction.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class AlarmClockAction : MonoBehaviour, IAction
 {
-
+    public PuzzleManager puzMan;
     public void DoAction()
     {
         GetComponentInChildren<AudioSource>().Stop();
+        puzMan.NextPuzzle();
     }
 }
